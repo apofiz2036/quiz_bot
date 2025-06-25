f6819de634e4ddc4653b173add0b2d901f631b9b
Apofiz <mikhailov_ya@mail.ru>
2025-06-25 07:44:30 +0500

    add keyboard

--- tg_bot.py
+++ tg_bot.py
@@ -2,43 +2,49 @@ import logging
 from dotenv import load_dotenv
 import os
 
-from telegram import Update, ForceReply
+from telegram import Update, ForceReply, ReplyKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
-load_dotenv()
-TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+QUIZ_KEYBOARD = [
+        ["Новый вопрос", "Сдаться"],
+        ["Мой счёт"]
+    ]
 
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
-)
-
-logger = logging.getLogger(__name__)
-
-
-def start(update: Update, context: CallbackContext) -> None:
-    user = update.effective_user
-    update.message.reply_markdown_v2(
-        fr'Hi {user.mention_markdown_v2()}\!',
-        reply_markup=ForceReply(selective=True),
+def start(update: Update, context: CallbackContext):
+    reply_markup = ReplyKeyboardMarkup(QUIZ_KEYBOARD, resize_keyboard=True)
+    update.message.reply_text(
+        "Привет! Я бот для викторин. Выбери действие:",
+        reply_markup=reply_markup
     )
 
 
-def help_command(update: Update, context: CallbackContext) -> None:
+def help_command(update: Update, context: CallbackContext):
     update.message.reply_text('Help!')
 
 
-def echo(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(update.message.text)
+def handle_buttons(update: Update, context: CallbackContext):
+    text = update.message.text
+    if text in ["Новый вопрос", "Сдаться", "Мой счёт"]:
+        update.message.reply_text('Привет. Я бот для викторин')
+    else:
+        update.message.reply_text(update.message.text)
 
 
-def main() -> None:
+def main():
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+    )
+    logger = logging.getLogger(__name__)
+
+    load_dotenv()
+    TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
     updater = Updater(TELEGRAM_TOKEN)
 
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("help", help_command))
 
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_buttons))
 
     updater.start_polling()
     updater.idle()
